5e4c9c86035d9b1d2a84a76efafb890c925b0d09
Sandip Nirmal <nirmal.sandip13389@gmail.com>
2020-11-04 22:06:35 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import Reveal from "reveal.js";
+import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
+import Revealhighlight from "reveal.js/plugin/highlight/highlight.esm.js";
+
+let deck = new Reveal({
+  plugins: [Markdown, Revealhighlight]
+});
+deck.initialize({ slideNumber: "c/t" });
