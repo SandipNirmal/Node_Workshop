e5d7a6e82e7e8b0bb4179e9cd585cbade21012b0
Sandip Nirmal <sandipnirmal@icloud.com>
2020-11-17 22:55:59 +0530

    Enabled speaker notes

--- dock/src/index.js
+++ dock/src/index.js
@@ -1,12 +1,12 @@
-import Reveal from 'reveal.js';
-import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
-import Revealhighlight from 'reveal.js/plugin/highlight/highlight.esm.js';
+import Reveal from "reveal.js";
+import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
+import Revealhighlight from "reveal.js/plugin/highlight/highlight.esm.js";
 
 let deck = new Reveal({
   plugins: [Markdown, Revealhighlight],
 });
 
 deck.initialize({
-  slideNumber: 'c/t',
-  // showNotes: true
+  slideNumber: "c/t",
+  showNotes: true,
 });
