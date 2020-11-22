b9b5c802e007d38812c527869d221ac4ed648848
Sandip Nirmal <sandipnirmal@icloud.com>
2020-11-22 20:18:14 +0530

    Fixed tweet APIs

--- app/src/middlewares/auth.js
+++ app/src/middlewares/auth.js
@@ -1,15 +1,15 @@
-import jwt from 'express-jwt';
+import jwt from "express-jwt";
 
-import { secret } from '../config';
+import { secret } from "../config";
 
 function getTokenFromHeader(req) {
   if (
     (req.headers.authorization &&
-      req.headers.authorization.split(' ')[0] === 'Token') ||
+      req.headers.authorization.split(" ")[0] === "Token") ||
     (req.headers.authorization &&
-      req.headers.authorization.split(' ')[0] === 'Bearer')
+      req.headers.authorization.split(" ")[0] === "Bearer")
   ) {
-    return req.headers.authorization.split(' ')[1];
+    return req.headers.authorization.split(" ")[1];
   }
 
   return null;
@@ -18,15 +18,15 @@ function getTokenFromHeader(req) {
 export const auth = {
   required: jwt({
     secret: secret,
-    userProperty: 'payload',
+    userProperty: "payload",
     getToken: getTokenFromHeader,
-    algorithms: ['HS256'],
+    algorithms: ["HS256"],
   }),
   optional: jwt({
     secret: secret,
-    userProperty: 'payload',
+    userProperty: "payload",
     credentialsRequired: false,
     getToken: getTokenFromHeader,
-    algorithms: ['HS256'],
+    algorithms: ["HS256"],
   }),
 };
--- app/src/modules/tweets/store.js
+++ app/src/modules/tweets/store.js
@@ -3,7 +3,7 @@ import { query_one, query } from "../../db";
 export async function addTweet({ id, userId, content }) {
   const addTweetQuery = `INSERT INTO tweets
   (id, user_id, text_content)
-  VALUES $1, $2, $3 returning id`;
+  VALUES ($1, $2, $3) returning id`;
 
   const values = [id, userId, content];
 
@@ -17,28 +17,28 @@ export async function addTweet({ id, userId, content }) {
 }
 
 export async function listTweets() {
-  const listTweets = `SELECT t.id, t.text_content, t.created, t.user_id, user.dispaly_name as author,
+  const listTweets = `SELECT t.id, t.text_content, t.user_id, ua.display_name as author, t.created
   FROM tweets t
-  INNER JOIN user_accounts user on t.user_id = user.id
+  INNER JOIN user_accounts ua on ua.id = t.user_id
   ORDER BY t.created DESC`;
 
   try {
     const { rows: tweets } = await query(listTweets, []);
 
-    return { tweets };
+    return tweets;
   } catch (e) {
     throw new Error(e);
   }
 }
 
 export async function getTweetById(id) {
-  const getTweet = `SELECT t.id, t.text_content, t.created, t.user_id, user.dispaly_name as author,
-    FROM tweets t
-    INNER JOIN user_accounts user on t.user_id = user.id
-    WHERE id=$1`;
+  const getTweet = `SELECT t.id, t.text_content, t.user_id, ua.display_name as author, t.created
+  FROM tweets t
+  INNER JOIN user_accounts ua on ua.id = t.user_id
+  WHERE t.id=$1`;
 
   try {
-    const tweet = await query_one(getTweet, []);
+    const { rowCount, ...tweet } = await query_one(getTweet, [id]);
 
     return tweet;
   } catch (e) {
